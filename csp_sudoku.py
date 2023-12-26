110ed0862b8159eed59cbd4e38a3e8a9e6c429f5
Noe Javet <nj4v3t@gmail.com>
2023-12-26 22:47:25 +0100

    csp sudoku

--- csp_sudoku.py
+++ csp_sudoku.py
@@ -7,7 +7,19 @@ a constraint satisfaction problem consists of three components:
 
 """
 import itertools
+import string
 import collections
+
+
+aima = [[0, 0, 3, 0, 2, 0, 6, 0, 0],
+        [9, 0, 0, 3, 0, 5, 0, 0, 1],
+        [0, 0, 1, 8, 0, 6, 4, 0, 0],
+        [0, 0, 8, 1, 0, 2, 9, 0, 0],
+        [7, 0, 0, 0, 0, 0, 0, 0, 8],
+        [0, 0, 6, 7, 0, 8, 2, 0, 0],
+        [0, 0, 2, 6, 0, 9, 5, 0, 0],
+        [8, 0, 0, 2, 0, 3, 0, 0, 9],
+        [0, 0, 5, 0, 1, 0, 3, 0, 0]]
 
 
 grid = [0, 0, 0, 0, 0, 6, 0, 7, 0,
@@ -23,20 +35,96 @@ grid = [0, 0, 0, 0, 0, 6, 0, 7, 0,
 
 class Agent:
     def __init__(self, grid):
-        self.state = self._preprocess_input(grid)
-        self.variables = self._get_variables()
-
-    @staticmethod
-    def _preprocess_input(grid):
-        return grid
-
-    def _get_variables(self):
-        return [ind for ind, val in enumerate(self.state) if val == 0]
+        self.grid = grid
+        self.zero_cells = self.get_zero_cells()
     
-    def _get_row(self, ind):
-        pass
-    def _get_col(self, ind):
-        pass
-    def _get_box(self, ind):
-        pass
+    def get_zero_cells(self):
+        zeros = {}
+        for i, row in enumerate(self.grid):
+            for j, val in enumerate(row):
+                if val == 0:
+                    zeros[i, j] = self.get_free_values(i, j)
+        return zeros
+
+    def get_neighbors(self, i, j):
+        neighbors = [(i, k) for k in range(0, i)]
+        neighbors = neighbors + [(i, k) for k in range(i+1, 9)]
+
+        neighbors = neighbors + [(k, j) for k in range(0, j)]
+        neighbors = neighbors + [(k, j) for k in range(j+1, 9)]
+
+        rs = i - i % 3
+        cs = j - j % 3
+        for ii in range(rs, rs + 3):
+            for jj in range(cs, cs + 3):
+                if (ii, jj) not in neighbors:
+                    if (ii, jj) != (i, j):
+                        neighbors.append((ii, jj))
+        return [(ii, jj) for ii, jj in neighbors if (ii, jj) in self.zero_cells.keys()]
+
+
+    def get_row_vals(self, i):
+        return self.grid[i]
+
+    def get_col_vals(self, j):
+        return [row[j] for row in self.grid]
+
+    def get_box_vals(self, i, j):
+        row_start = i - i % 3
+        col_start = j - j % 3
+        box = []
+        for ii in range(row_start, row_start+3):
+            for jj in range(col_start, col_start+3):
+                box.append(self.grid[ii][jj])
+        return box
+
+    def get_free_values(self, i, j):
+        a = self.get_row_vals(i)
+        b = self.get_col_vals(j)
+        c = self.get_box_vals(i, j)
+        d = set(a + b + c)
+        return [val for val in range(1, 10) if val not in d]
+
+    def ac3(self, queue):
+        while queue:
+            i, j, ii, jj = queue.pop()
+            if self.revise(i, j, ii, jj):
+                if len(self.zero_cells[i, j]) == 0:
+                    return False
+                for x, y in self.get_neighbors(i, j):
+                    if (x, y) != (ii, jj):
+                        queue.append((x, y, i, j))
+
+        return True
+
+    def revise(self, i, j, ii, jj):
+        revised = False
+        di = self.zero_cells[i, j]
+        dj = self.zero_cells[ii, jj]
+        for x in di:
+            if len(dj) == 1 and dj[0] == x:
+                ind = di.index(x)
+                del di[ind]
+                revised = True
+        return revised
+
+    def solve(self):
+        queue = []
+        for i, j in self.zero_cells.keys():
+            neighbors = self.get_neighbors(i, j)
+            for ii, jj in neighbors:
+                queue.append((i, j, ii, jj))
+        self.ac3(queue)
+
+
+
+agent = Agent(aima)
+
+for (i, j), val in agent.zero_cells.items():
+    print(i, j, val)
+
+agent.solve()
+print('---------')
+for (i, j), val in agent.zero_cells.items():
+    print(i, j, val)
 
--- sudoku.py
+++ sudoku.py
@@ -2,17 +2,6 @@ import itertools
 import collections
 
 
-grid0 = [[3, 4, 0, 2, 9, 8, 7, 0, 1],
-        [9, 6, 0, 0, 5, 0, 0, 0, 0],
-        [8, 7, 0, 6, 0, 1, 5, 9, 3],
-        [1, 0, 0, 0, 2, 0, 0, 0, 0],
-        [0, 0, 7, 8, 0, 0, 0, 4, 9],
-        [5, 0, 8, 0, 0, 0, 1, 0, 6],
-        [0, 0, 9, 0, 0, 0, 4, 0, 0],
-        [0, 0, 0, 0, 7, 2, 0, 1, 5],
-        [7, 0, 0, 1, 0, 4, 0, 0, 0]]
-
-
 grid = [[0, 0, 0, 0, 2, 0, 5, 0, 0],
         [0, 0, 8, 5, 0, 9, 0, 0, 2],
         [0, 4, 0, 3, 8, 0, 1, 9, 0],
@@ -23,151 +12,79 @@ grid = [[0, 0, 0, 0, 2, 0, 5, 0, 0],
         [1, 2, 0, 8, 9, 5, 0, 0, 0],
         [0, 0, 0, 4, 0, 0, 0, 0, 0]]
 
+amia = [[0, 0, 3, 0, 2, 0, 6, 0, 0],
+        [9, 0, 0, 3, 0, 5, 0, 0, 1],
+        [0, 0, 1, 8, 0, 6, 4, 0, 0],
+        [0, 0, 8, 1, 0, 2, 9, 0, 0],
+        [7, 0, 0, 0, 0, 0, 0, 0, 8],
+        [0, 0, 6, 7, 0, 8, 2, 0, 0],
+        [0, 0, 2, 6, 0, 9, 5, 0, 0],
+        [8, 0, 0, 2, 0, 3, 0, 0, 9],
+        [0, 0, 5, 0, 1, 0, 3, 0, 0]]
+
+
 class Agent:
     def __init__(self):
         self.grid = grid
-        self.variables = self.get_variables()
+        self.moves = 0
 
-    def get_variables(self):
-        variables = []
+    def get_zero_cell(self):
         for i, row in enumerate(self.grid):
             for j, val in enumerate(row):
                 if val == 0:
-                    variables.append((i, j))
-        return variables
-
-    def get_row(self, i):
-        return self.grid[i]
-
-    def get_col(self, j):
-        return [row[j] for row in self.grid]
-
-    def get_box(self, i, j):
-        if 0 <= i < 3:
-            row_ind = [0, 1, 2]
-        if 3 <= i < 6:
-            row_ind = [3, 4, 5]
-        if 6 <= i < 9:
-            row_ind = [6, 7, 8]
-
-        if 0 <= j < 3:
-            col_ind = [0, 1, 2]
-        if 3 <= j < 6:
-            col_ind = [3, 4, 5]
-        if 6 <= j < 9:
-            col_ind = [6, 7, 8]
+                    return i, j
     
-        return [self.grid[ri][ci]
-                for (ri, ci) in itertools.product(row_ind, col_ind)]
+    def is_safe_row(self, i, val):
+        return val not in self.grid[i]
+    
+    def is_safe_col(self, j, val):
+        return val not in [row[j] for row in self.grid]
 
-    def get_free_values(self, i, j):
-        row = self.get_row(i)
-        col = self.get_col(j)
-        box = self.get_box(i, j)
-        vals = set(row + col + box)
-        return [val for val in range(1, 10) if not val in vals]
+    def is_safe_box(self, i, j, val):
+        row_start = i - i % 3
+        row_ind = [row_start, row_start + 1, row_start + 2]
+        col_start = j - j % 3
+        col_ind = [col_start, col_start + 1, col_start + 2]
+        return val not in [self.grid[ri][ci] 
+                           for (ri, ci) in itertools.product(row_ind, col_ind)]
+
+    def is_safe(self, i, j, val):
+        return (
+                self.is_safe_row(i, val) and
+                self.is_safe_col(j, val) and
+                self.is_safe_box(i, j, val)
+        )
 
     def print_board(self):
-        print(' | '.join([str(self.grid[0:3]), str(self.grid[3:6]), str(self.grid[6:])]))
+        for i, row in enumerate(self.grid):
+            print(' | '.join([' '.join([str(val) for val in row[0:3]]),
+                              ' '.join([str(val) for val in row[3:6]]),
+                              ' '.join([str(val) for val in row[6:]])]))
+            if (i + 1) % 3 == 0:
+                print('---------------------')
 
     def solve(self):
-        for ind, (i, j) in enumerate(self.variables):
-            free_values = self.get_free_values(i, j)
-            # solution is certain 
-            if len(free_values) == 1:
-                self.grid[i][j] = free_values[0]
-            # simulation
-            else:
-                for val in free_values:
-                    self._simulate(i, j, ind, val)
+        try:
+            i, j = self.get_zero_cell()
+        except TypeError:
+            return True
 
-    def _simulate(self, i, j, ind, val):
-        print('board', self.grid)
-        print('root: ', i, j, val)
-        self.grid[i][j] = val
-        branch = [(i, j)]
-        for ii, jj in self.variables[ind+1:]:
-            free_values = self.get_free_values(ii, jj)
-            if len(free_values) == 1:
-                self.grid[ii][jj] = free_values[0]
-                branch.append((ii, jj))
-            elif len(free_values) == 0:
-                print('contradiction at', i, j)
-                print('zeroing branch', branch)
-                for wi, wj in branch:
-                    self.grid[wi][wj] = 0
-            else:
-                for fv in free_values:
-                    self._simulate(ii, jj, ind+1, fv)
+        for val in range(1, 10):
+            if self.is_safe(i, j, val):
+                self.grid[i][j] = val
+                self.moves += 1
 
+                if self.solve():
+                    return True
 
-
-
-
-
-
+                self.grid[i][j] = 0
     
 
 
 ag = Agent()
-#ag.solve()
-def solve_sudoku(board):
-    empty = find_empty(board)
-    
-    # If there are no empty cells, the puzzle is solved
-    if not empty:
-        return True
-
-    row, col = empty
-
-    # Try filling the empty cell with numbers 1 to 9
-    for num in range(1, 10):
-        if is_safe(board, row, col, num):
-            # If the number is safe, assign it to the cell
-            board[row][col] = num
-
-            # Recursively attempt to solve the rest of the puzzle
-            if solve_sudoku(board):
-                return True
-
-            # If the current assignment does not lead to a solution, backtrack
-            board[row][col] = 0
-
-    # If no number can be placed, backtrack to the previous cell
-    return False
-
-def find_empty(board):
-    # Find the first empty cell (with value 0)
-    for i in range(9):
-        for j in range(9):
-            if board[i][j] == 0:
-                return (i, j)
-    return None
-
-def is_safe(board, row, col, num):
-    # Check if the number is not present in the row, column, and 3x3 subgrid
-    return (
-        is_safe_row(board, row, num) and
-        is_safe_col(board, col, num) and
-        is_safe_box(board, row - row % 3, col - col % 3, num)
-    )
-
-def is_safe_row(board, row, num):
-    return num not in board[row]
-
-def is_safe_col(board, col, num):
-    return all(row[col] != num for row in board)
-
-def is_safe_box(board, start_row, start_col, num):
-    return all(
-        num not in board[i][start_col:start_col + 3]
-        for i in range(start_row, start_row + 3)
-    )
-
-if solve_sudoku(grid):
-    for row in grid:
-        print(row)
-
+ag.solve()
+ag.print_board()
+print('backtrack', ag.moves)
 
 """
 AIMA problem solving agents 
