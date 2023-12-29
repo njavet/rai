7dbea7b63b58e2a2cff5b0a0712524dc7547e752
Noe Javet <nj4v3t@gmail.com>
2023-12-29 12:34:00 +0100

    agent skeletons

---
+++ agent.py
@@ -0,0 +1,38 @@
+class Environment:
+    pass
+
+
+class Sensor:
+    pass
+
+
+class Percept:
+    pass
+
+
+class Actuator:
+    pass
+
+
+class Action:
+    pass
+
+
+class Rule:
+    pass
+
+
+class State:
+    pass
+
+
+class Agent:
+    def __init__(self):
+        self.sensors = None
+        self.actuators = None
+
+    def agent_program(self, percept):
+        """ returns an action """ 
+
+
+
