2632965d3d617adf22fee46fdcc6fa4a984cdce9
Noe Javet <nj4v3t@gmail.com>
2024-01-11 13:16:00 +0100

    trial and error 2048

--- g2048/agent.py
+++ g2048/agent.py
@@ -10,12 +10,6 @@ UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
 MOVES = [UP, DOWN, LEFT, RIGHT]
 
 
-# goals:
-# highest tile in left upper corner
-# as many empty cells as possible
-# as many merges as possible 
-
-
 class SimMove:
     def __init__(self, move, grid):
         self.move = move
@@ -26,9 +20,14 @@ class SimMove:
         self.zeros = self.number_of_zeros()
         self.merges = self.number_of_merges()
         # primary selection criteria
-        self.td_dix = self.tile_distances()
-        self.md = self.max_distance()
-        self.td = list(self.td_dix.values())
+        self.dmax = self.max_distance()
+        self.mscore = self.score()
+        self.td = self.tile_distances()
+        self.dall = 0
+        self.d1 = 10
+        self.d2 = 10
+        self.d3 = 10
+        self.d4 = 10
 
     def gen_tile_dict(self):
         dix = collections.defaultdict(list)
@@ -48,10 +47,14 @@ class SimMove:
         return len(self.tile_val_pos.get(0, []))
 
     def score(self):
+        if not self.merges:
+            return 0
+
         dix = collections.defaultdict(int)
         for row in self.grid:
             for val in row:
                 dix[val] += 1
+
         new_dix = collections.defaultdict(int)
         for row in self.new_grid:
             for val in row:
@@ -68,56 +71,79 @@ class SimMove:
     def max_distance(self):
         try:
             mv = max(self.tile_val_pos.keys())
-            pos = self.tile_val_pos[mv]
+            pos = self.tile_val_pos.get(mv)
+            #pos = self.tile_val_pos[mv]
             return sum(pos[0])
         except ValueError:
             return 0
 
     def tile_distances(self):
         self.tile_val_pos.pop(0, None)
-        #self.tile_val_pos.pop(2, None)
-        #self.tile_val_pos.pop(4, None)
-        #self.tile_val_pos.pop(8, None)
+        named_tiles = {}
+        for i, tile in enumerate(self.tile_val_pos.keys()):
+            named_tiles['t' + str(i + 1)] = self.tile_val_pos[tile]
 
-        tile_dist = collections.defaultdict(int)
-        for tile, positions in self.tile_val_pos.items():
+        dix = {}
+        for name, positions in named_tiles.items():
             # all pairs of tile with the same value
             if len(positions) == 1:
-                tile_dist[tile] = sum(positions[0])
+                # drive towards upper left, TODO good ?
+                dix[name] = sum(positions[0])
+            elif len(positions) == 2:
+                i0, j0 = positions[0]
+                i1, j1 = positions[1]
+                dd = (abs(i0 - i1) + abs(j0 - j1))
+                if name == 't1':
+                    self.d1 = dd
+                if name == 't2':
+                    self.d2 = dd
+                elif name == 't3':
+                    self.d3 = dd
+                elif name == 't4':
+                    self.d4 = dd
+                else:
+                    dix[name] = dd
             else:
+                total = 0
                 for (i0, j0), (i1, j1) in itertools.combinations(positions, r=2):
-                    tile_dist[tile] += abs(i0 - i1) + abs(j0 - j1)
-        return tile_dist
-
-
-class State:
-    def __init__(self, grid):
-        self.grid = copy.deepcopy(grid)
-        self.simmoves = self.compute_grids()
-
-    def compute_grids(self):
-        simmoves = {}
-        for move in MOVES:
-            sm = SimMove(move, self.grid)
-            if sm.has_effect():
-                simmoves[move] = sm
-        return simmoves
-
-    def chose_move(self):
-        lst = sorted(self.simmoves.values(), key=lambda sm: (sm.md,
-                                                             (-1)*sm.score(),
-                                                             sm.td,
-                                                             (-1)*sm.merges,
-                                                             (-1)*sm.zeros))
-
-        for s in lst:
-            tds = ' '.join([f'{v:1.1f}' for v in s.td])
-            print('score', s.score(), 'TD', tds, '\tMERGES', s.merges, 'ZEROS', s.zeros)
-        return lst[0].move
+                    total += (abs(i0 - i1) + abs(j0 - j1))
+                dix[name] = total
+        return dix
 
 
 def find_best_move(grid):
-    return State(grid).chose_move()
+    simmoves = []
+    for move in MOVES:
+        sm = SimMove(move, grid)
+        if sm.has_effect():
+            simmoves.append(sm)
+
+    for sm in simmoves:
+        sm.dall = sum(sm.td.values())
+
+    lst = sorted(simmoves, key=lambda sim: (sim.dmax,
+                                            (-1)*sim.mscore,
+                                            sim.d1,
+                                            sim.d2,
+                                            sim.d3,
+                                            sim.d4,
+                                            sim.dall,
+                                            (-1)*sim.merges,
+                                            (-1)*sim.zeros))
+
+    for s in lst:
+#        tds = ' '.join([f'{v:1.1f}' for v in s.td])
+        print('dmax', s.dmax,
+              'score', s.mscore,
+              'd1', s.d1,
+              'd2', s.d2,
+              'd3', s.d3,
+              'd4', s.d4,
+              'all', s.dall,
+              'merges', s.merges,
+              'zeros', s.zeros)
+
+    return lst[0].move
 
 
 def random_move():
