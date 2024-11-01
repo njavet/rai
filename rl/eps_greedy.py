ea8dfb892f9931bf45cdfd8a630de3663881445e
Noe Javet <javetnoe@students.zhaw.ch>
2024-11-01 20:19:44 +0100

    implement eps greedy

--- rl/eps_greedy.py
+++ rl/eps_greedy.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class EpsilonGreedy:
@@ -5,12 +6,14 @@ class EpsilonGreedy:
         self.epsilon = epsilon
 
     def choose_action(self, action_space, state, qtable):
-        """TODO: Implement the e-greedy algorithm. i.e.:
+        """ TODO: Implement the e-greedy algorithm. i.e.:
             with probability epsilon:
                 select an action randomly
             else
-                select the action with the highest q-value"""
+                select the action with the highest q-value """
+        if np.random.rand() < self.epsilon:
+            action = action_space.sample()
+        else:
+            action = np.argmax(qtable[state])
 
-        # Select a random action
-        action = action_space.sample()
         return action
