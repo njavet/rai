b7ed848fe272e434ba2cc941f506173781dbcd26
Noe Javet <javetnoe@students.zhaw.ch>
2024-11-01 20:18:15 +0100

    implement q learning

--- rl/q_learning.py
+++ rl/q_learning.py
@@ -10,13 +10,17 @@ class Qlearning:
         self.reset_qtable()
         self.qtable = np.zeros((self.state_size, self.action_size))
 
-
     def update(self, state, action, reward, new_state):
-        """TODO: Change the following code to implement the update of the Q-function
+        """ TODO: Change the following code to implement the update of the Q-function
             Q_update(s,a):= Q(s,a) + learning_rate * delta
-                delta =  [R(s,a) + gamma * max Q(s',a') - Q(s,a)]"""
+                delta =  [R(s,a) + gamma * max Q(s',a') - Q(s,a)] """
 
-        q_update = self.qtable[state, action]
+        # Compute the temporal difference (TD) target
+        best_future_q = np.max(self.qtable[new_state])  # max Q(s', a')
+        delta = reward + self.gamma * best_future_q - self.qtable[state, action]
+
+        # Update the Q-value
+        q_update = self.qtable[state, action] + self.learning_rate * delta
         return q_update
 
     def reset_qtable(self):
