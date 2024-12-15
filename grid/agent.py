8b9b77e314d3c7c307772c5f9e322d629336e0bf
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-15 14:21:57 +0100

    pydantic models

--- grid/agent.py
+++ grid/agent.py
@@ -1,50 +1,85 @@
-from collections import defaultdict
+from enum import Enum
+from pydantic import BaseModel
 
-# project imports
-from grid.environment import Action, State, Reward
+
+class State(BaseModel):
+    x: int
+    y: int
+
+
+class Action(Enum):
+    # finite set of actions
+    UP = -1, 0
+    RIGHT = 0, 1
+    DOWN = 1, 0
+    LEFT = 0, -1
+
+
+class TrajectoryElement(BaseModel):
+    state: State
+    reward: float
+    action: Action
 
 
 class Agent:
     def __init__(self):
-        self.state_t = 3, 0
-        self.reward_t = 0
-        self.policy = [[2, 2, 2, 2, 3],
-                       [1, 0, 2, 2, 3],
-                       [1, 0, 1, 0, 0],
-                       [0, 2, 2, 2, 1],
-                       [2, 2, 2, 2, 1]]
-        self.value = [[-6, -5, -4, -3, -2],
-                      [-7, 0, -3, -2, -1],
-                      [-8, 0, -4, 0, 0],
-                      [0, -4, -3, -2, -1],
-                      [-6, -5, -4, -3, -2]]
-        self.reward = [[-1, -1, -1, -1, -1],
-                       [-1, 0, -1, -1, -1],
-                       [-1, 0, -1, 0, -1],
-                       [0, -1, -1, -1, -1],
-                       [-1, -1, -1, -1, -1]]
+        self.state = 3, 0
+        self.terminal_state = 2, 4
+        self.model = [[1, 1, 1, 1, 1],
+                      [1, 0, 1, 1, 1],
+                      [1, 0, 1, 0, 1],
+                      [1, 1, 1, 1, 1],
+                      [1, 1, 1, 1, 1]]
+        self.gamma = 1
 
-    def _policy(self, state: State) -> Action:
-        """
-        the policy p(a|s) = P[At = a | St = s]
-        maps an action At to a given state St where At has probability P
+    def generate_trajectories(self):
+        lst = []
 
-        """
+        def helper(state_t, action_t, traj):
+            for action in Action:
+                cond0 = self.is_inverse_action(action, action_t)
+                cond1 = self.is_valid_action(action)
+                state_t1 =
+                if cond0 and cond1:
+                    traj.append(state_t)
+                    traj.append(self.reward())
+                    traj.append(state_t)
+
+    def is_inverse_action(self, action0, action1):
+        ax0, ay0 = action0.value
+        ax1, ay1 = action1.value
+        return sum([ax0, ay0, ax1, ay1]) == 0
+
+    def is_valid_action(self, action):
+        ax, ay = action.value
+        x, y = self.state
+        new_x = ax + x
+        new_y = ay + y
+        if new_x < 0 or new_x > 5:
+            return False
+        if new_y < 0 or new_y > 5:
+            return False
+        if self.model[new_x][new_y] == 0:
+            return False
+        return True
+
+    @staticmethod
+    def reward(state, action, next_state):
+        return -1
+
+    def transition(self, action):
+        x, y = self.state
+        ax, ay = action.value
+        xs = self.model[x] + ax
+        ys = self.model[x][y] + ay
+        self.state = xs, ys
+
+    def policy(self):
         pass
 
-    def value_function(self, state: State) -> float:
-        """
-        The value function computes the expectation value given a policy
-        of the total reward in this state: V = E[sum(rewards) | St = s]
-
-        """
+    def value_function(self):
         pass
 
-    def predict_next_state(self, state: State, action: Action) -> State:
+    def q(self, action):
         pass
-
-    def predict_next_reward(self, state: State, action: Action) -> Reward:
-        pass
-
-
 
--- grid/environment.py
+++ grid/environment.py
@@ -1,32 +1,7 @@
-from enum import Enum
 
+# markov decision process
+# 0 is an invalid state
+# 1 is a valid state
+# 2 is the initial state
+# 3 is the terminal state
 
-class Environment:
-    def __init__(self):
-        self.grid = [[1, 1, 1, 1, 1],
-                     [1, 0, 1, 1, 1],
-                     [1, 0, 1, 0, 1],
-                     [1, 1, 1, 1, 1],
-                     [1, 1, 1, 1, 1]]
-        self.state = State(3, 0)
-
-
-class State:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def get_coordinates(self):
-        return self.x, self.y
-
-
-class Reward:
-    def __init__(self):
-        self.r = -1
-
-
-class Action(Enum):
-    UP = 1
-    RIGHT = 2
-    DOWN = 3
-    LEFT = 4
