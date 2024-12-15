9000d962f5c8b887cf8dcd020c7da38d133641b4
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-15 10:19:00 +0100

    add opening action

--- tictactoe/agent.py
+++ tictactoe/agent.py
@@ -1,11 +1,87 @@
+import collections
+import json
 import random
 
 
 class Agent:
-    def __init__(self, player):
-        self.player = player
+    def __init__(self,
+                 init_state,
+                 exploration_rate=0.1,
+                 step_size=0.1,
+                 sym='X') -> None:
+        self.exploration_rate = exploration_rate
+        self.step_size = step_size
+        self.sym = sym
+        self.model = self._load_model('model.json')
+        self.state = init_state
+        self.action = Action(self.state, self.exploration_rate)
+
+    @staticmethod
+    def _load_model(file_name):
+        with open(file_name) as f:
+            model = json.load(f)
+        return model
+
+    @staticmethod
+    def _save_model(model, file_name):
+        with open(file_name, 'w') as f:
+            json.dump(model, f, indent=2)
 
 
 class Action:
-    def __init__(self):
-        pass
+    def __init__(self, state, exploration_rate):
+        self.state = state
+        self.exploration_rate = exploration_rate
+
+    def get_action(self):
+        if self.state.is_init():
+            return self._opening_action()
+
+        free_cells = self.state.get_free_cells()
+        if random.uniform(0, 1) < self.exploration_rate:
+            return random.choice(free_cells)
+
+        best_prob = 0
+        chosen_field = free_fields[0]
+        for ind, field in enumerate(free_fields):
+
+            new_board = engine.insert_symbol(board, turn, field)
+            win_prob = model.get(new_board, 0.5)
+
+            if best_prob < win_prob:
+                best_prob = win_prob
+                chosen_field = field
+        return chosen_field
+
+    @staticmethod
+    def _opening_action():
+        """ three equivalent actions:
+        corner, edge, center """
+        opening = random.random()
+        # corner
+        if opening < 0.33:
+            # 4 equivalent corners
+            corner = random.random()
+            if corner < 0.25:
+                return 0
+            elif corner < 0.5:
+                return 2
+            elif corner < 0.75:
+                return 6
+            else:
+                return 8
+        # edge
+        if 0.33 <= opening < 0.66:
+            # 4 equivalent edges
+            edge = random.random()
+            if edge < 0.25:
+                return 1
+            elif edge < 0.5:
+                return 3
+            elif edge < 0.75:
+                return 5
+            else:
+                return 7
+        # center
+        else:
+            return 4
--- tictactoe/env.py
+++ tictactoe/env.py
@@ -4,16 +4,28 @@ from rich.console import Console
 class Env:
     def __init__(self):
         self.board = 9 * ' '
+        self.state = State(self.board)
+        self.reward = Reward(self.board)
 
 
 class State:
-    def __init__(self):
-        pass
+    def __init__(self, board):
+        self.board = board
+
+    def is_init(self):
+        return self.board == 9 * ' '
+
+    def get_free_cells(self):
+        free_cells = []
+        for i, cell in enumerate(self.board):
+            if cell == ' ':
+                free_cells.append(i)
+        return free_cells
 
 
 class Reward:
-    def __init__(self):
-        pass
+    def __init__(self, board):
+        self.board = board
 
 
 class EnvPres:
