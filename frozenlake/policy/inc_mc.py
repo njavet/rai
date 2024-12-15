12a3d824ea4f77f6d7b3e649e8a3950d485f7fc5
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-15 17:39:11 +0100

    incremental monte carlo

---
+++ frozenlake/policy/inc_mc.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# project imports
+from frozenlake.policy.base import BasePolicy
+from frozenlake.helpers import argmax
+
+
+class MonteCarloIncPolicy(BasePolicy):
+    def __init__(self, action_space, obs_space_size, action_space_size, eps):
+        super().__init__()
+        self.eps = eps
+
+    def choose_action(self, state):
+        """ with probability epsilon:
+                select an action randomly
+            else
+                select the action with the highest q-value """
+        if np.random.rand() < self.eps:
+            action = self.action_space.sample()
+        else:
+            action = argmax(self.qtable[state])
+        return action
