ddd8bd861c55055e81ae96e1bda0b3e93409baf1
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-15 09:50:52 +0100

    base setup

--- tictactoe/agent.py
+++ tictactoe/agent.py
@@ -4,3 +4,8 @@ import random
 class Agent:
     def __init__(self, player):
         self.player = player
+
+
+class Action:
+    def __init__(self):
+        pass
--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,37 +1,22 @@
 import argparse
 
-# project imports
-from state import State
-from agent import Agent
-
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--games', type=int, default=100, dest='games')
+    parser.add_argument('-g', dest='games', default=1000, type=int)
+    parser.add_argument('-m', dest='tmode', default=1, type=int)
     return parser
-
-
-def play_game(game, training_mode=False):
-    s = State()
-
-    while not s.game_over:
-        if s.whos_turn() == 'O':
-            s.pretty_print()
-            move = int(input('Enter your next move'))
-            while not s.is_valid_move(move):
-                move = int(input('invalid move... try again:'))
-            s.execute_move(move, 'O')
-            s.is_game_over()
-
 
 
 def main():
     parser = create_parser()
     args = parser.parse_args()
 
-    for game in args.games:
-        play_game(game)
+    # training mode, self play
+    if args.tmode:
+        pass
 
+    # play against another agent
+    else:
+        pass
 
-if __name__ == '__main__':
-    main()
