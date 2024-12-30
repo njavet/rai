1539c142fe2fce71d761d19b1d0565e1fd2594f2
Noe Javet <np.javet@gmail.com>
2024-12-30 14:41:46 +0100

    refactoring

--- rai/agents/base.py
+++ rai/agents/base.py
@@ -1,4 +1,9 @@
 from abc import ABC
+import numpy as np
+from collections import defaultdict
+
+# project imports
+from rai.utils.models import Trajectory, TrajectoryStep
 
 
 class SchopenhauerAgent(ABC):
@@ -12,3 +17,75 @@ class SchopenhauerAgent(ABC):
         """ params could be seen as given by nature / god"""
         self.env = env
         self.params = params
+
+
+class Learner(SchopenhauerAgent):
+    def __init__(self, env, params):
+        super().__init__(env, params)
+        self.vtable = np.zeros(params.state_size)
+        self.qtable = np.zeros((params.state_size, params.action_size))
+        self.trajectories = defaultdict(list)
+
+    def reset_q_table(self):
+        self.qtable = np.zeros((self.params.state_size, self.params.action_size))
+
+    def get_action(self, state: int) -> int:
+        raise NotImplementedError
+
+    def exec_step(self, state: int, action: int) -> tuple[int, TrajectoryStep, bool]:
+        next_state, reward, term, trunc, info = self.env.step(action)
+        ts = TrajectoryStep(state=state, action=int(action), reward=reward)
+        done = term or trunc
+        return next_state, ts, done
+
+    def process_step(self):
+        pass
+
+    def process_episode(self, episode):
+        pass
+
+    def process_episodes(self):
+        pass
+
+    def update_qtable(self, *args):
+        pass
+
+    def generate_trajectory(self) -> Trajectory:
+        trajectory = Trajectory(steps=[])
+        state, info = self.env.reset()
+        done = False
+        while not done:
+            action = self.get_action(state)
+            next_state, ts, done = self.exec_step(state, action)
+            state = next_state
+            trajectory.steps.append(ts)
+            # the agent might want to do something after each step
+            self.process_step()
+        return trajectory
+
+    def evaluate_trajectory(self, trajectory: Trajectory) -> tuple[np.ndarray, np.ndarray]:
+        returns = np.zeros((self.params.state_size, self.params.action_size))
+        counts = np.zeros((self.params.state_size, self.params.action_size))
+        total_reward = 0
+        for t in reversed(trajectory.steps):
+            state, action, reward = t.state, t.action, t.reward
+            total_reward += reward
+            returns[state, action] += total_reward
+            counts[state, action] += 1
+        return returns, counts
+
+    def run_env(self):
+        qtables = np.zeros((self.params.n_runs,
+                            self.params.state_size,
+                            self.params.action_size))
+        for n in range(self.params.n_runs):
+            self.reset_q_table()
+            for episode in range(self.params.total_episodes):
+                trajectory = self.generate_trajectory()
+                self.trajectories[episode].append(trajectory)
+                # the agent might want to do something after each episode
+                self.process_episode(episode)
+            # the agent might want to do something after all episodes
+            self.process_episodes()
+            qtables[n, :, :] = self.qtable
+        self.qtable = qtables.mean(axis=0)
