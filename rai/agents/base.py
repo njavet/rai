8cffa385ce3dcb98d892fa3c8e4f83c620eb9c68
Noe Javet <np.javet@gmail.com>
2024-12-30 13:32:45 +0100

    add base schopenhauer agent

---
+++ rai/agents/base.py
@@ -0,0 +1,14 @@
+from abc import ABC
+
+
+class SchopenhauerAgent(ABC):
+    """
+    For now lets define a SchopenhauerAgent as an Agent
+    that has an environment as part of himself. So the environment exists
+    only inside the agent. Another type would be a Cartesian Agent that is
+    part of the environment. The third Agent type would be a mix of both.
+    """
+    def __init__(self, env, params):
+        """ params could be seen as given by nature / god"""
+        self.env = env
+        self.params = params
