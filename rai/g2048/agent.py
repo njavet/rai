855805d5d00b51d28c18f9d719ca476bc39ba724
Noe Javet <np.javet@gmail.com>
2024-12-30 20:38:35 +0100

    add random arg max

--- rai/g2048/agent.py
+++ rai/g2048/agent.py
@@ -1,20 +1,24 @@
 import random
 import numpy as np
 
-from rai.g2048.expectimax import expectimax, simulate_move
+# project imports
+from rai.g2048.expectimax import expectimax, simulate_move, is_move_available
+from rai.utils.helpers import random_argmax
 
 
 def find_best_move(grid):
-    result = [score_top_level_move(i, grid) for i in range(4)]
+    result = np.array([score_top_level_move(i, grid) if is_move_available(grid, i)
+                      else 0 for i in range(4)])
+    print(result)
 
-    if max(result) == 0:
+    if np.max(result) == 0:
         move = random.choice([0, 1, 2, 3])
     else:
-        move = result.index(max(result))
+        move = random_argmax(result)
     return move
 
 
-def score_top_level_move(move, grid, depth=4):
+def score_top_level_move(move, grid, depth=3):
     new_grid, _ = simulate_move(grid, move)
     if np.equal(new_grid, grid).all():
         return 0
