8a42cdc804bdb205ed2eeba4721095861c8de615
Noe Javet <np.javet@gmail.com>
2024-12-30 13:45:05 +0100

    refactoring

--- rai/agents/rl_agent.py
+++ rai/agents/rl_agent.py
@@ -1,10 +1,11 @@
 import numpy as np
 from collections import defaultdict
+from typing import Callable
 
 # project imports
 from rai.agents.base import SchopenhauerAgent
 from rai.utils.helpers import random_argmax
-from rai.utils.models import Params
+from rai.utils.models import Params, TrajectoryStep, Trajectory
 
 
 class RLAgent(SchopenhauerAgent):
@@ -14,15 +15,48 @@ class RLAgent(SchopenhauerAgent):
         self.qtable = np.zeros((params.state_size, params.action_size))
         self.trajectories = defaultdict(list)
 
-    def get_optimal_action(self, state: int) -> int:
+    def get_action(self, state: int) -> int:
         action = random_argmax(self.qtable[state])
         return action
 
-    def exec_step(self, state: int, action: int) -> tuple[int, GymTrajectory, bool]:
+    def exec_step(self, state: int, action: int) -> tuple[int, TrajectoryStep, bool]:
         next_state, reward, term, trunc, info = self.env.step(action)
-        ts = GymTrajectory(state=state, action=int(action), reward=reward)
+        ts = TrajectoryStep(state=state, action=int(action), reward=reward)
         done = term or trunc
         return next_state, ts, done
+
+    def process_step(self):
+        pass
+
+    def generate_trajectory(self) -> Trajectory:
+        trajectory = Trajectory(steps=[])
+        state, info = self.env.reset()
+        done = False
+        while not done:
+            action = self.get_action(state)
+            next_state, ts, done = self.exec_step(state, action)
+            state = next_state
+            trajectory.steps.append(ts)
+            self.process_step()
+        return trajectory
+
+    def run_env(self):
+        pass
+
+
+
+    def run(self):
+        qtables = np.zeros((self.params.n_runs,
+                            self.params.state_size,
+                            self.params.action_size))
+        for n in range(self.params.n_runs):
+            self.reset_tables()
+            for episode in range(self.params.total_episodes):
+                trajectory = self.generate_trajectory(self.get_action)
+                self.trajectories[episode].append(trajectory)
+            qtables[n, :, :] = self.qtable
+        self.qtable = qtables.mean(axis=0)
+
 
 class Learner(RLAgent):
     def __init__(self, env, params):
