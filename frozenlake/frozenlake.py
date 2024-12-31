516fa6b6ec4a76e3b120e16d375ba7eecf6a9bc2
Noe Javet <np.javet@gmail.com>
2024-12-31 12:51:09 +0100

    merge preparation

--- frozenlake/frozenlake.py
+++ frozenlake/frozenlake.py
@@ -1,25 +1,8 @@
 import seaborn as sns
-import gymnasium as gym
-import numpy as np
-from gymnasium.envs.toy_text.frozen_lake import generate_random_map
-import time
 
 # project imports
-from frozenlake.config import get_params
-from frozenlake import helpers
-from frozenlake.rmc import run
-
-
-def get_env(params):
-    env = gym.make(
-        'FrozenLake-v1',
-        is_slippery=params.is_slippery,
-        render_mode="rgb_array",
-        desc=generate_random_map(size=params.map_size,
-                                 p=params.proba_frozen,
-                                 seed=params.seed))
-
-    return env
+from frozenlake.utils.helpers import get_params, get_env
+from frozenlake.rmc import Agent
 
 
 def main():
@@ -28,12 +11,9 @@ def main():
     params = get_params()
     env = get_env(params)
 
-    trajectories = run(env, params)
-    for i, trajectory in enumerate(trajectories):
-        print('trajectory', i)
-        for state, reward, action in trajectory:
-            print('state', state, 'reward', reward, 'action', action)
-        print(78*'-')
+    agent = Agent(env, params)
+    agent.run()
+    agent.pprint_q_table()
 
 
 if __name__ == '__main__':
--- frozenlake/rmc.py
+++ frozenlake/rmc.py
@@ -1,26 +1,66 @@
+from rich.console import Console
 from collections import defaultdict
+import numpy as np
+
+# project imports
+from frozenlake.utils.helpers import rand_argmax
 
 
-def run(env, params):
-    trajectories = []
-    values = defaultdict(float)
-    counts = defaultdict(float)
-    action_value = defaultdict(float)
+class Agent:
+    def __init__(self, env, params):
+        self.env = env
+        self.params = params
+        self.counts = defaultdict(int)
+        self.values = defaultdict(float)
+        self.qtable = np.zeros((env.observation_space.n, env.action_space.n))
+        self.trajectories = None
+        self.console = Console()
 
-    for episode in range(params.total_episodes):
+    def run(self):
+        self.trajectories = {}
+        for episode in range(self.params.total_episodes):
+            self.run_episode(episode)
+            self.learn(episode)
+
+    def state_action_value(self, state, action):
+        key = state, action
+        try:
+            value = self.values[key] / self.counts[key]
+        except ZeroDivisionError:
+            value = 0
+        return value
+
+    def learn(self, i):
+        total_reward = 0
+        for state, reward, action in self.trajectories[i][::-1]:
+            key = state, action
+            total_reward += reward
+            self.counts[key] += 1
+            self.values[key] += total_reward
+
+        for state, action in self.values.keys():
+            self.qtable[state, action] = self.state_action_value(state, action)
+
+    def run_episode(self, i):
+        state, info = self.env.reset()
         trajectory = []
-        done = False
-        state, info = env.reset()
-        while not done:
-            action = env.action_space.sample()
-            next_state, reward, term, trunc, info = env.step(action)
+        while True:
+            action = self.env.action_space.sample()
+            next_state, reward, term, trunc, info = self.env.step(action)
             trajectory.append((state, reward, action))
             state = next_state
-            if trunc:
-                print('trunc', next_state, reward, action)
-            elif term:
-                print('term', next_state, reward, action)
-            done = term or trunc
+            if trunc or term:
+                break
+        self.trajectories[i] = trajectory
 
-        trajectories.append(trajectory)
-    return trajectories
+    def pprint_q_table(self):
+        for i in range(4):
+            self.console.print(16*'-', style='cyan')
+            values = []
+            for j in range(4):
+                val = rand_argmax(self.qtable[i + j])
+                values.append(f'{val:.2f}')
+            self.console.print(' | '.join(values))
+        self.console.print(16*'-', style='cyan')
+
+        print(self.values, self.counts)
