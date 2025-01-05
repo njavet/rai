01145470ae5cbf30f4443594ca7bc1e3be70b412
Noe Javet <np.javet@gmail.com>
2025-01-05 13:02:34 +0100

    change param design

--- rai/rl/agents/learner.py
+++ rai/rl/agents/learner.py
@@ -1,22 +1,24 @@
 from abc import ABC
 from gymnasium.spaces import Discrete
-import torch
 
 # project imports
-from rai.utils.models import LearnerParams, Trajectory
+from rai.utils.models import Trajectory
 
 
 class Learner(ABC):
-    def __init__(self, params: LearnerParams, action_space: Discrete, model=None):
-        self.params = params
-        self.model = model
+    def __init__(self,
+                 state_space: Discrete,
+                 action_space: Discrete,
+                 params=None):
+        self.state_space = state_space
         self.action_space = action_space
+        self.params = params
         self.trajectory: Trajectory = Trajectory()
 
     def reset_trajectory(self) -> None:
         self.trajectory = Trajectory()
 
-    def policy(self, state: torch.Tensor) -> torch.Tensor:
+    def policy(self, state: int) -> int:
         raise NotImplementedError
 
     def process_step(self) -> None:
