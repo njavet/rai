4eeb565d9026aa9dd340019d4b5ec9267b4045c5
Noe Javet <np.javet@gmail.com>
2025-01-05 12:53:27 +0100

    dp value estimation

--- rai/__init__.py
+++ rai/__init__.py
@@ -2,7 +2,7 @@ from .rl.envs.grid import GridEnv
 from gymnasium.envs.registration import register
 
 register(
-    id='rai/rai/rl/envs/Game2048-v0',
+    id='rai/GridEnv-v0',
     entry_point='rai:GridEnv',
 )
 
--- rai/rl/agents/dp.py
+++ rai/rl/agents/dp.py
@@ -6,48 +6,72 @@ from rai.rl.agents.learner import Learner
 from rai.utils.helpers import random_argmax
 
 
-class DP(Learner):
+class DP:
     """ dynamic programming learner """
-    def __init__(self, params, action_space, model):
-        super().__init__(params, action_space, model)
-        self.gamma = params.gamma
-        self.eps = params.epsilon
-        self.eps_min = params.epsilon_min
-        self.decay = params.decay
+    def __init__(self):
         # self.qtable = np.zeros((params.state_size, action_space.n))
-        self.vtable = np.zeros((16, action_space.n))
+        self.vtable = np.zeros(16)
+        self.policy = defaultdict(int)
 
-    def policy(self, state):
-        epsilon = max(self.eps_min, self.eps * self.decay)
-        if np.random.rand() < epsilon:
-            action = self.action_space.sample()
+    @staticmethod
+    def reward_table(state, next_state):
+        if (state, next_state) in [(0, 1),
+                                     (1, 2),
+                                     (2, 3),
+                                     (3, 7),
+                                     (7, 6),
+                                     (6, 5),
+                                     (5, 4),
+                                     (4, 8),
+                                     (8, 9),
+                                     (9, 10),
+                                     (10, 11),
+                                     (11, 15)]:
+            return 1
         else:
-            action = random_argmax(self.qtable[state])
-        return action
+            return 0
 
-    def process_episode(self, episode):
-        total_reward = 0
-        visited_state_actions = set()
-        for ts in reversed(self.trajectory.steps):
-            s, a, r = ts.state, ts.action, ts.reward
-            total_reward = self.gamma * total_reward + r
-            if (s, a) not in visited_state_actions:
-                visited_state_actions.add((s, a))
-                self.returns[(s, a)].append(total_reward)
-        for (s, a), rewards in self.returns.items():
-            if rewards:
-                self.qtable[s, a] = np.mean(rewards)
+    def pos_to_tuple(self, state):
+        """ convert integer position to grid position"""
+        x, y = divmod(state, 4)
+        return x, y
 
-    def learn(self):
-        qtables = np.zeros((self.params.n_runs,
-                            self.params.state_size,
-                            self.params.action_size))
-        for n in range(self.params.n_runs):
-            self.qtable = np.zeros((self.params.state_size, self.params.action_size))
-            for episode in range(self.params.total_episodes):
-                self.generate_trajectory()
-                # collect all trajectories
-                self.trajectories[(n, episode)] = self.trajectory
-                self.process_trajectory(episode)
-                qtables[n, :, :] = self.qtable
-        self.qtable = np.mean(qtables, axis=0)
+    @staticmethod
+    def get_action_values(action):
+        if action == 0:
+            return 0, -1
+        elif action == 1:
+            return 1, 0
+        elif action == 2:
+            return 0, 1
+        elif action == 3:
+            return -1, 0
+        raise ValueError('invalid action')
+
+    def get_next_state(self, state, action):
+        x, y = self.pos_to_tuple(state)
+        ax, ay = self.get_action_values(action)
+        x1 = max(x + ax, 0)
+        y1 = max(y + ay, 0)
+        x_new = min(x1, 3)
+        y_new = min(y1, 3)
+        return x_new * 4 + y_new
+
+    def compute_optimal_value_function(self, h):
+        # v0
+        self.vtable = np.zeros(16)
+        for i in range(h):
+            # vk = max(R(state, action, nextstate) + gamma * vk-1(nextstate)
+            for state in range(16):
+                lst = []
+                for action in range(4):
+                    ns = self.get_next_state(state, action)
+                    r = self.reward_table(state, ns)
+                    curr = r + self.vtable[ns]
+                    lst.append((action, r, curr))
+                lst = sorted(lst, key=lambda x: x[1], reverse=True)
+                a, r, v = lst[0]
+                print('state', state, 'action', a, 'v', v)
+                self.vtable[state] = v
+                self.policy[state] = a
+        return self.vtable.reshape((4, 4))
