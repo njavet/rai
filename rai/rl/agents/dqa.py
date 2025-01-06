a36ac7adf0f6742ccc432c01e203559e1c1d4c6c
Noe Javet <np.javet@gmail.com>
2025-01-06 13:14:51 +0100

    move to multi env

--- rai/rl/agents/dqa.py
+++ rai/rl/agents/dqa.py
@@ -1,16 +1,16 @@
 from collections import deque
 import random
 import gymnasium as gym
+import numpy as np
 import torch
 import torch.optim as optim
 
 
 # project imports
-from rai.rl.agents.learner import Learner
 from rai.rl.dqn import DQN
 
 
-class DQNAgent(Learner):
+class DQNAgent:
     def __init__(self,
                  env: gym.Env,
                  n_runs: int,
@@ -29,7 +29,9 @@ class DQNAgent(Learner):
         self.action_dim = env.action_space.n
         self.policy_net = DQN(self.obs_dim, self.action_dim).to(self.dev)
         self.target_net = DQN(self.obs_dim, self.action_dim).to(self.dev)
-        self.memory = ReplayMemory(capacity=memory_size)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        # self.target_net.eval()
+        self.memory = ReplayMemory(self.dev, capacity=memory_size)
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
         self.batch_size = batch_size
         self.target_update_steps = target_update_steps
@@ -37,6 +39,16 @@ class DQNAgent(Learner):
         self.epsilon = epsilon
         self.min_epsilon = min_epsilon
         self.decay = decay
+        self.steps = 0
+
+    def select_actions(self, states):
+        if random.random() < self.epsilon:
+            return np.random.rand(self.action_dim, len(states))
+        states = torch.tensor(states, dtype=torch.float32, device=self.dev)
+        with torch.no_grad():
+            q_values = self.policy_net(states)
+        actions = q_values.argmax(dim=1).detach().cpu().numpy()
+        return actions
 
     def epsilon_decay(self):
         self.epsilon = max(self.epsilon * self.decay, self.min_epsilon)
@@ -44,31 +56,55 @@ class DQNAgent(Learner):
     def process_step(self):
         if len(self.memory) < self.batch_size:
             return
-        self.memory.push(self.trajectory[-1])
-        batch = self.memory.sample(self.batch_size)
+        states, actions, rewards, next_states, dones = self.memory.sample(self.batch_size)
+        q_values = self.policy_net(states).gather(1, actions).squeeze()
+        with torch.no_grad():
+            next_q_values = self.target_net(next_states).max(1)[0]
+        expected_q_values = rewards + (self.gamma * next_q_values * (1 - dones))
+        loss = torch.nn.functional.mse_loss(q_values, expected_q_values)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-
-        states, action, rewards, next_states, dones =
-        q_values = self.policy_net(states).gather(1, actions)
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.policy_net.state_dict())
 
     def process_episode(self, episode):
-        pass
+        self.epsilon_decay()
 
     def learn(self):
         pass
 
 
 class ReplayMemory:
-    def __init__(self, capacity: int):
+    def __init__(self, device: str, capacity: int):
+        self.device = device
         self.memory = deque(maxlen=capacity)
 
-    def push(self, *args):
-        self.memory.append(args)
+    def store_transitions(self, states, actions, rewards, next_states, dones):
+        transitions = zip(states, actions, rewards, next_states, dones)
+        for transition in transitions:
+            self.memory.append(transition)
 
     def sample(self, batch_size):
         batch = random.sample(self.memory, batch_size)
-        ts for ts in batch
-        ts.state
+        states, actions, rewards, next_states, dones = zip(*batch)
+        states = torch.tensor(np.array(states),
+                              dtype=torch.float32,
+                              device=self.device)
+        actions = torch.tensor(np.array(actions),
+                               dtype=torch.long,
+                               device=self.device).unsqueeze(1)
+        rewards = torch.tensor(np.array(rewards),
+                               dtype=torch.float32,
+                               device=self.device)
+        next_states = torch.tensor(np.array(next_states),
+                                   dtype=torch.float32,
+                                   device=self.device)
+        dones = torch.tensor(np.array(dones),
+                             dtype=torch.float32,
+                             device=self.device)
+        return states, actions, rewards, next_states, dones
 
     def __len__(self):
         return len(self.memory)
