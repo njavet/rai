9eb2509e41d92ef81491c785380d14227c9fb9ef
Noe Javet <np.javet@gmail.com>
2025-01-06 10:58:47 +0100

    generalize trajectory step

--- rai/utils/models.py
+++ rai/utils/models.py
@@ -1,12 +1,16 @@
 from pydantic import BaseModel, Field
+import torch
 
 
 class TrajectoryStep(BaseModel):
-    state: int
-    action: int
-    reward: float
-    next_state: int
-    done: bool
+    state: int | torch.tensor
+    action: int | torch.tensor
+    reward: float | torch.tensor
+    next_state: int | torch.tensor
+    done: bool | torch.tensor
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
 class Trajectory(BaseModel):
